86293dfc0432236aafab1cfd0bb0f95bb7c1d28b
karthikSingireddy <skarthikreddy189@gmail.com>
2022-06-18 20:46:23 -0400

    initial

---
+++ rest-api/task_list/__init__.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+def create_app():
+  app = Flask(__name__)
+  app.config.from_mapping(
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key'
+  )
+
+  return app
+
+
